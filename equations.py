881b4317f4d3850875f15ac8cdd155921234a85b
Matthieu Chau <matthieu.chau@eleves.enpc.fr>
2024-03-24 18:51:52 +0100

    discretized dynamics

--- equations.py
+++ equations.py
@@ -5,20 +5,34 @@ n = 2 * np.pi / (24 * 60 * 60)  # Earth angular's velocity (rad/s)
 m = 3000  # mass (kg)
 dt = 20 * 60  # dt in seconds
 total_time = 7 * 24 * 60 * 60  # total time of one week in seconds
-num_steps = int(total_time / dt)  # iterations
+num_steps = total_time//dt  # iterations
 
 
-# function to calculate the next state, necassary for gymnasium step function
-def next_state(t, s, control, dt):
-    x, y, z, x_dot, y_dot, z_dot, q1, q2, q3, q4, w1, w2, w3 = s
+# discretized clohessy-wiltshire equations
+def clohessy_wiltshire(r, v, dt, F):
+    # finite differences
+    ax = (v[0] - 2*n*v[1]) / dt + F[0] / m
+    ay = (v[1] + 2*n*v[0]) / dt + F[1] / m
+    az = (v[2] - n**2*r[1]) / dt + F[2] / m
 
-    Fx, Fy, Fz, Lx, Ly, Lz = control(t)
+    a = np.array([ax, ay, az])
 
-    x_ddot = 3 * n**2 * x + 2 * n * y_dot + Fx / m
-    y_ddot = -2 * n * x_dot + Fy / m
-    z_ddot = -n**2 * z + Fz / m
+    new_r = r + v*dt + 0.5*a*dt**2
+    new_v = v + a*dt
 
-    # flake8: noqa
-    s_dot = np.array([x_dot, y_dot, z_dot, x_ddot, y_ddot, z_ddot, 0, 0, 0, 0, 0, 0, 0])
+    return np.array([new_r, new_v])
 
-    return s + dt * s_dot
+def next_q(q, dt):
+    qx, qy, qz, qw = q
+    Omega = np.array([[-qx, -qy, -qz], [qw, -qz, qy], [qz, qw, -qx], [-qy, qx, qw]])
+    # finite differences
+    q_dot = 0.5 * np.dot(Omega, q)
+    new_q = q + q_dot*dt
+    return new_q
+
+def next_w(w, L, dt):
+    J = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])# à remplacer par le vrai J
+    # finite differences
+    w_dot = np.dot(np.linalg.inv(J), L-np.dot(w, np.dot(J, w)))
+    new_w = w + w_dot*dt
+    return new_w
